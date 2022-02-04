8189d0cf828473b36f922607763983f862b87547
daleschembri <44404912+daleschembri@users.noreply.github.com>
2022-02-04 23:00:35 +0100

    Creation

---
+++ src/constants/config.ts
@@ -0,0 +1,15 @@
+export const CONFIG = {
+  tries: 6, // This changes how many tries you get to finish the wordle
+  language: 'Maltese', // This changes the display name for your language
+  wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
+  author: 'Dale Schembri', // Put your name here so people know who made this Wordle!
+  authorWebsite: 'daleschembri.com', // Put a link to your website or social media here
+  wordListSource: 'maltese-dictionary', // Describe the source material for your words here
+  wordListSourceLink: 'https://github.com/shaunazzopardi/maltese-dictionary', // Put a link to the source material for your words here
+  //
+  // THESE NEXT SETTINGS ARE FOR ADVANCED USERS
+  //
+  googleAnalytics: 'G-69XTF9V4XV', // You can use this if you use Google Analytics
+  shuffle: false, // whether to shuffle the words in the wordlist each time you load the app (note: you will lose the 'word of the day' functionality if this is true)
+  normalization: 'NFC', // whether to apply Unicode normalization to words and orthography - options: 'NFC', 'NFD', 'NKFC', 'NKFD', false
+}
---
+++ src/constants/orthography.ts
@@ -0,0 +1,40 @@
+import { CONFIG } from './config'
+
+export const ORTHOGRAPHY = [
+  'ċ',
+  'q',
+  'w',
+  'e',
+  'r',
+  't',
+  'y',
+  'u',
+  'i',
+  'o',
+  'p',
+  'ġ',
+  'a',
+  's',
+  'd',
+  'f',
+  'g',
+  'h',
+  'j',
+  'k',
+  'l',
+  'z',
+  'x',
+  'c',
+  'v',
+  'b',
+  'n',
+  'm',
+  'ħ',
+  'ż',
+]
+
+if (CONFIG.normalization) {
+  ORTHOGRAPHY.forEach(
+    (val, i) => (ORTHOGRAPHY[i] = val.normalize(CONFIG.normalization))
+  )
+}
